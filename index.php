c933d3552e21920f9b40293fd1f3a2b6d1a6e924
Ramin Vakilian <ramin@aligent.com.au>
2015-05-27 09:56:13 +0930

    initial commit

---
+++ index.php
@@ -0,0 +1,116 @@
+
+<!DOCTYPE html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Know who: Coming Soon Page</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.css" />
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js" type="text/javascript"></script>
+    <script src="js/jquery.backgroundpos.min.js" type="text/javascript"></script>
+    <script src="js/jquery.countdown.min.js" type="text/javascript"></script>
+    <script src="js/scripts.js" type="text/javascript"></script>
+
+</head>
+
+<body>
+<!--Header Block-->
+<div id="hello">
+    <img src="images/logo.png" alt="Company Logo" width="200" height="48" />
+    <h3>Yes! We are currently working hard on awesome new site.</h3>
+    <h1>Stay tuned!</h1>
+    <h4>Coming in December 2013</h4>
+</div>
+
+<!--Construction area pictures-->
+<div id="container">
+    <div id="clouds">
+        <div id="background-buildings">
+            <div id="background-small-cranes">
+                <div id="background-vehicles">
+                    <div id="background-road-seal">
+                        <div id="lorries">
+                            <div id="foreground-ground">
+                                <div id="container-counter">
+                                    <div id="defaultCountdown">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<!--Here is container with email subscription-->
+<div class="container-fluid yellow-container bg-mail">
+    <div class="row-fluid">
+        <div class="span12">
+            <h2>Be the first who will know our secrets!</h2>
+            <p>Just enter your email address to get exclusive invitation.</p>
+
+            <div class="input-append">
+                <form action="save.php" id="subscribe-form" method="post" name="subscribe-form">
+                    <input type="email" name="email" placeholder="Enter your email" id="email" class="email required" />
+                    <button class="btn btn-inverse" id="submit" type="submit" value="Subscribe">Subscribe</button>
+                </form>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<!--Here is container with "What is this?" and Contact information-->
+<div class="container-fluid">
+    <div class="row-fluid">
+        <div class="span4">
+            <h3>What is this?</h3>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vitae augue erat. Praesent et fringilla ipsum. Phasellus eleifend, arcu et pharetra finibus, enim ex imperdiet sem, in imperdiet sem magna ut orci. Proin ornare dapibus nisl ac varius. Morbi ligula libero, interdum vel felis et, fermentum dignissim mi. Nunc id sapien tincidunt, cursus nulla nec, volutpat libero. Proin ac ante nec ipsum eleifend fermentum. Integer volutpat massa neque. Proin vitae dui tristique, feugiat purus eget, vestibulum dui.
+        </div>
+        <div class="span4">
+            <h3>What we offer?</h3>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vitae augue erat. Praesent et fringilla ipsum. Phasellus eleifend, arcu et pharetra finibus, enim ex imperdiet sem, in imperdiet sem magna ut orci. Proin ornare dapibus nisl ac varius. Morbi ligula libero, interdum vel felis et, fermentum dignissim mi. Nunc id sapien tincidunt, cursus nulla nec, volutpat libero. Proin ac ante nec ipsum eleifend fermentum. Integer volutpat massa neque. Proin vitae dui tristique, feugiat purus eget, vestibulum dui.
+        </div>
+        <div class="span4">
+            <h3>How to contact?</h3>
+            <ul>
+                <li>Phone: (+61) 0433 571 565</li>
+                <li>E-mail: <a href="mailto:daniel@incaroads.com">daniel@incaroads.com</a></li>
+                <li>Website: <a href="http://www.whoknow.com.au">www.whoknow.com.au</a></li>
+                <li>Adelaide, SA 5000</li>
+            </ul>
+        </div>
+    </div>
+
+
+    <!-- Social Media Buttons - CSS3 - Free to Remove Unneeded -->
+    <div class="social-buttons">
+        <ul class="social">
+            <li><a href="http://facebook.com/"><i class="icon-facebook-circled"></i></a></li>
+            <li><a href="http://linkedin.com/"><i class="icon-linkedin-circled"></i></a></li>
+            <li><a href="http://google.com/"><i class="icon-gplus-circled"></i></a></li>
+            <li><a href="http://twitter.com/"><i class="icon-twitter-circled"></i></a></li>
+            <li><a href="http://pinterest.com/"><i class="icon-pinterest-circled"></i></a></li>
+            <li><a href="http://tumblr.com/"><i class="icon-tumblr-circled"></i></a></li>
+            <li><a href="http://stumbleupon.com/"><i class="icon-stumbleupon-circled"></i></a></li>
+            <li><a href="http://dribble.com/"><i class="icon-dribbble-circled"></i></a></li>
+            <li><a href="http://vimeo.com/"><i class="icon-vimeo-circled"></i></a></li>
+            <li><a href="http://mixi.com/"><i class="icon-mixi"></i></a></li>
+            <li><a href="http://youtube.com/"><i class="icon-youtube"></i></a></li>
+            <li><a href="http://lastfm.com/"><i class="icon-lastfm-circled"></i></a></li>
+            <li><a href="http://instagram.com/"><i class="icon-instagram"></i></a></li>
+            <li><a href="http://vk.com/"><i class="icon-vkontakte"></i></a></li>
+            <li><a href="http://flickr.com/"><i class="icon-flickr-circled"></i></a></li>
+        </ul>
+    </div>
+
+</div>
+</body>
+</html>
